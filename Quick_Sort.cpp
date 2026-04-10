ce98693bb10fb0c24383814e71af49af3a430800
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-04-11 00:57:43 +0600

    Quick Sort

--- Quick_Sort.cpp
+++ Quick_Sort.cpp
@@ -1,51 +1,77 @@
 ///Bismillahir Rahmanir Rahim
 //Quick sort implementation
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int partition(int a[], int l, int r)
+vector<int> generate(int size, int min_val = 0, int max_val = 100000)
 {
-    int i = l, j = r - 1, pivot = a[r];
-    while(i < j){
-        if(a[i] > pivot){
-            swap(a[i], a[j]);
-            j--;
-        }
-        else{
-            i++;
-        }
+    vector<int> dataset;
+    dataset.reserve(size);
+    for (int i = 0; i < size; ++i)
+    {
+        int range = max_val - min_val + 1;
+        int num = min_val + (rand() % range);
+        dataset.push_back(num);
     }
-    if(a[i] > pivot){
-        swap(a[i], a[r]);
-        return i;
-    }
-    else{
-        swap(a[i+1], a[r]);
-        return i+1;
-    }
+    return dataset;
 }
 
-void QuickSort(int a[], int l, int r)
+void print(const vector<int> &data)
 {
-    if(l >= r) return;
-    int m = partition(a, l, r);
-    QuickSort(a, l, m-1);
-    QuickSort(a, m+1, r);
+    cout << "Generated " << data.size() << " numbers:\n";
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        cout << data[i];
+        if (i != data.size() - 1)
+            cout << " ";
+    }
+    cout << "\n";
+    cout << "Student ID: C243042 \n"
+         << "Student Name: Ahmed Ibrahim Sajid\n";
+}
+int partition(vector<int> &arr, int start, int end)
+{
+    int pivot = arr[end];
+    int i = start - 1;
+
+    for (int j = start; j < end; j++)
+    {
+        if (arr[j] <= pivot)
+        {
+            i++;
+            swap(arr[i], arr[j]);
+        }
+    }
+    i++;
+    swap(arr[i], arr[end]);
+    return i;
+}
+
+void quicksort(vector<int> &arr, int start, int end)
+{
+    if (start < end)
+    {
+        int p = partition(arr, start, end);
+        quicksort(arr, start, p - 1);
+        quicksort(arr, p + 1, end);
+    }
 }
 
 int main()
 {
-    int n, arr[100];
-    cin >> n;
-    for(int i = 1; i <= n; i++){
-        cin >> arr[i];
-    }
+    srand(static_cast<unsigned>(time(nullptr)));
 
-    QuickSort(arr, 1, n);
+    int size = 1000;
+    vector<int> data = generate(size);
 
-    for(int i = 1; i <= n; i++){
-        cout << arr[i] << " ";
-    }
+    auto start_time = chrono::high_resolution_clock::now();
 
-    return 0;
+    quicksort(data, 0, size - 1);
+
+    auto end_time = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, std::milli> execution_time = end_time - start_time;
+
+    print(data);
+    cout << "Execution Time: " << execution_time.count() << " ms\n";
 }
