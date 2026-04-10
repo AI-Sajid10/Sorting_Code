07e2dccc3c990a011da3d2ea43d5120fb16a3b37
Sajid <c243042@ugrad.iiuc.ac.bd>
2026-04-11 00:55:28 +0600

    Merge_sort six

--- Merge_Sort.cpp
+++ Merge_Sort.cpp
@@ -1,56 +1,97 @@
 ///Bismillahir Rahmanir Rahim
 //merge sort implementation
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-
-void Merge(int c[], int l, int m, int r)
+vector<int> generate(int size, int min_val = 0, int max_val = 100000)
 {
-    vector<int> a, b;
-    for(int k = l; k <= m; k++)
-        a.push_back(c[k]);
-    for(int k = m+1; k <= r; k++)
-        b.push_back(c[k]);
-
-    int i = 0, j = 0;
-    for(int k = l; k <= r; k++)
+    vector<int> dataset;
+    dataset.reserve(size);
+    for (int i = 0; i < size; ++i)
     {
-        if(i < a.size() && (j >= b.size() || a[i] <= b[j]))
-            c[k] = a[i++];
+        int range = max_val - min_val + 1;
+        int num = min_val + (rand() % range);
+        dataset.push_back(num);
+    }
+    return dataset;
+}
+
+void print(const vector<int> &data)
+{
+    cout << "Generated " << data.size() << " numbers:\n";
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        cout << data[i];
+        if (i != data.size() - 1)
+            cout << " ";
+    }
+    cout << "\n";
+    cout << "Student ID: C243014 \n"
+         << "Student Name: Nafizur Rahman Seam\n";
+}
+
+void merge(vector<int> &arr, int start, int mid, int end)
+{
+    vector<int> temp;
+    int i = start;
+    int j = mid + 1;
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] <= arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
         else
-            c[k] = b[j++];
+        {
+            temp.push_back(arr[j]);
+            j++;
+        }
+    }
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+    while (j <= end)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    for (int idx = 0; idx < temp.size(); idx++)
+    {
+        arr[idx + start] = temp[idx];
     }
 }
 
-void MergeSort(int c[], int l, int r)
+void mergesort(vector<int> &arr, int start, int end)
 {
-    if(l < r)
+    if (start < end)
     {
-        int m = (l + r) / 2;
-        MergeSort(c, l, m);
-        MergeSort(c, m + 1, r);
-        Merge(c, l, m, r);
+        int mid = start + (end - start) / 2;
+
+        mergesort(arr, start, mid);
+        mergesort(arr, mid + 1, end);
+
+        merge(arr, start, mid, end);
     }
 }
 
 int main()
 {
-    int n;
-    cout << "Enter number of elements: ";
-    cin >> n;
+    srand(static_cast<unsigned>(time(nullptr)));
 
-    int c[n];
-    cout << "Enter " << n << " elements:\n";
-    for(int i = 0; i < n; i++)
-        cin >> c[i];
+    int size = 1000;
+    vector<int> data = generate(size);
 
-    MergeSort(c, 0, n - 1);
+    auto start_time = chrono::high_resolution_clock::now();
 
-    cout << "Sorted array:\n";
-    for(int i = 0; i < n; i++)
-        cout << c[i] << " ";
-    cout << endl;
+    mergesort(data, 0, size - 1);
 
-    return 0;
+    auto end_time = chrono::high_resolution_clock::now();
+
+    chrono::duration<double, std::milli> execution_time = end_time - start_time;
+
+    print(data);
+    cout << "Execution Time: " << execution_time.count() << " ms\n";
 }
-
-
